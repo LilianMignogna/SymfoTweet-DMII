cc5f008dbcd14ec810062d42845aa969cf30a159
LMignogna <lmignogna@altimax.com>
2022-02-19 14:06:13 +0100

    Add view + fix register form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,26 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/number.html.twig', [
+        return $this->render('home/index.html.twig', [
+            "home" => "home",
+        ]);
+    }
+
+    #[Route('/symfotweets', name: 'symfotweets_list')]
+    public function list(): Response
+    {
+        // ...
+        return $this->render('home/listing.html.twig', [
+            "home" => "home",
+        ]);
+    }
+
+    #[Route('/symfotweetos/{symfotweetos_id}', name: 'symfotweetos')]
+    #[ParamConverter('symfotweetos_id', options: ['mapping' => ['symfotweetos_id' => 'id']])]
+    public function show(string $symfotweetos_id): Response
+    {
+        // ...
+        return $this->render('home/profile.html.twig', [
             "home" => "home",
         ]);
     }
