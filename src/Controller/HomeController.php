d615d4178e9b98c94dc4f3d3b348ca8b576945ac
LMignogna <lmignogna@altimax.com>
2022-02-19 15:03:52 +0100

    Add Login form + logout + post symfotweet WIP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,10 @@
 // src/Controller/HomeController.php
 namespace App\Controller;
 
+use App\Entity\Symfotweets;
+use App\Form\Type\SymfotweetsFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +17,27 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
+            "home" => "home",
+        ]);
+    }
+
+    #[Route('/new-symfotweet', name: 'post_symfotweet')]
+    public function newSymfotweet(Request $request): Response
+    {
+        $symfoTweets = new Symfotweets();
+
+        $form = $this->createForm(SymfotweetsFormType::class, $symfoTweets);
+
+        return $this->render('home/new_symfotweet.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/profile', name: 'profile')]
+    public function profile(): Response
+    {
+
+        return $this->render('home/profile.html.twig', [
             "home" => "home",
         ]);
     }
---
+++ src/Form/Type/SymfotweetsFormType.php
@@ -0,0 +1,22 @@
+<?php
+
+// src/Form/Type/SymfotweetsFormType.php
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SymfotweetsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextType::class)
+            ->add('postdate', DateType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
+}
